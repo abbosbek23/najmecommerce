dbd4fbfbff02bfe274c395cb5c4b4a0ed9765063
abbosbek23 <muratovabbosbek@gmail.com>
2024-08-21 06:24:22 +0500

    changed codes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-import { defineConfig } from 'vite';
-
 export default defineConfig({
-  resolve: {
-    alias: {
-      'swiper/react': 'swiper/react',
-    },
-  },
-});
+  plugins: [react()],
+})
